a566f6c556ef3f912ccb58e02430f8ae0e52ef6b
Michał Niemiec <m.niemiec@lgbs.pl>
2016-11-20 16:45:04 +0100

    quick_union

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -11,11 +11,19 @@ public class UF {
 		}
 	}
 	
-	public void union(int p, int q) {
-	}
-	
-	public boolean connected(int p, int q) {
-		return false;
-	}
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
+    }
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
 	
 }
